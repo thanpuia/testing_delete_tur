e0c8cc5160e959336b07694ff11f7dc20aaccf3c
thanpuia <thanpuia46@gmail.com>
2019-07-25 12:14:32 +0530

    Add files via upload

---
+++ testing_delete_tur/src/main/java/com/testing_delete_tur/git/testing_delete_tur/TestingDeleteTurApplication.java
@@ -0,0 +1,13 @@
+package com.testing_delete_tur.git.testing_delete_tur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingDeleteTurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingDeleteTurApplication.class, args);
+	}
+
+}
---
+++ testing_delete_tur/src/test/java/com/testing_delete_tur/git/testing_delete_tur/TestingDeleteTurApplicationTests.java
@@ -0,0 +1,16 @@
+package com.testing_delete_tur.git.testing_delete_tur;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestingDeleteTurApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
